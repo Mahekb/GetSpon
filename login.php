c720ba2ed9f847570668da27b50a2f1d02865a84
Jainam321 <59683462+Jainam321@users.noreply.github.com>
2020-08-29 23:52:21 +0530

    Update login.php

--- login.php
+++ login.php
@@ -8,24 +8,26 @@
     <?php
             $nameErr="";
         ?>  
-    <ul>
-    <li><a class="left"><img src="Images/Mainlogo.jpg" width="100" </a></li>
-        <li><a class="left" href="http://localhost/getspon/Home_page.php">Home</a></li>
+    
+<ul>
+        <li><a class="left"><img src="Images/Mainlogo.jpg" width="100" </a></li>
+        <li><a class="left" href="http://localhost/Getspon/Home_page.php">Home</a></li>
         <li><a class="left" href="#About">About</a></li>
         <li><a class="left" href="#Contact">Contact</a></li>
         <li><a class="right" href="Log out">Log out</a></li>
-        <li><a class="right" href="http://localhost/getspon/Signup.php">Sign up</a></li>
-        <li><a class="right" href="http://localhost/getspon/login.php">Log in</a></li>
-</ul>
-    <br><br>
+        <li><a class="right" href="http://localhost/Getspon/Signup.php">Sign up</a></li>
+        <li><a class="right" href="http://localhost/Getspon/Login.php">Log in</a></li>
+
+</ul><br><br>
+
     <div align="center" id="log">
         <h1>Login</h1>
         <img src="Images/login.jpg" height = "150" width="150">
-    <form action="action_page.php" method="post" enctype="multipart/form-data" autocomplete="on" >
+    <form method="post" enctype="multipart/form-data" autocomplete="on" >
     <form method=post >
         <input type = "text"  name = "username" placeholder="Enter user-id" class="input-box" required><br><br>
         <input type="password" name="password" placeholder="Enter password" class="input-box" required><br><br>
-    <Text class="right"><a href="http://localhost/getspon/Signup.php">Forgot Password?</a></Text><br><br>
+    <Text class="right"><a href="http://localhost/Getspon/Signup.php">Forgot Password?</a></Text><br><br>
        
     <span class="error">* <?php echo $nameErr;?></span><br>
 
@@ -34,19 +36,19 @@
 
 </form>
    
-<Text>Don't have an account? <a href="http://localhost/getspon/Signup.php">Sign Up</a></Text><br><br>
+<Text>Don't have an account? <a href="http://localhost/Getspon/Signup.php">Sign Up</a></Text><br><br>
 <?php
             $un=$_GET["username"];
             $ps=$_GET["password"];
             if(isset($_POST["username"]) && isset($_POST["password"])){
                 if($un==$_POST["username"] && $ps==$_POST["password"]){
-                    header("Location: http://localhost/getspon/Home_page.php");
+                    header("Location: http://localhost/Getspon/Home_page.php");
                 }else{
                    $nameErr="Invalid credentials";
                }
             }
-        ?>
-<Text>Don't have an account? <a href="http://localhost/getspon/Signup.php">Sign Up</a></Text><br><br>
+?>
+
 
 <Text>-------------------------OR-------------------------</Text><br><br>
 
