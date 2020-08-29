e5d6c3d41cebea3fec4f722f215a6c47c1aaee57
Jainam321 <59683462+Jainam321@users.noreply.github.com>
2020-08-29 23:36:29 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,10 +3,6 @@
 <head>
     <title>Login</title>
     <link rel="stylesheet" type="text/css" href="mystyle.css">
-
-    <style>
-        
-    </style>
 </head>
 <body>
     <?php
@@ -20,40 +16,42 @@
         <li><a class="right" href="Log out">Log out</a></li>
         <li><a class="right" href="http://localhost/getspon/Signup.php">Sign up</a></li>
         <li><a class="right" href="http://localhost/getspon/login.php">Log in</a></li>
-
 </ul>
     <br><br>
     <div align="center" id="log">
         <h1>Login</h1>
         <img src="Images/login.jpg" height = "150" width="150">
+    <form action="action_page.php" method="post" enctype="multipart/form-data" autocomplete="on" >
     <form method=post >
         <input type = "text"  name = "username" placeholder="Enter user-id" class="input-box" required><br><br>
         <input type="password" name="password" placeholder="Enter password" class="input-box" required><br><br>
     <Text class="right"><a href="http://localhost/getspon/Signup.php">Forgot Password?</a></Text><br><br>
+       
     <span class="error">* <?php echo $nameErr;?></span><br>
 
 <button class="button" type="submit">Login</button><br><br>
 <input type="checkbox"  name="remember"> Remember me<br><br>
 
 </form>
+   
+<Text>Don't have an account? <a href="http://localhost/getspon/Signup.php">Sign Up</a></Text><br><br>
 <?php
-            $un=$_GET["uname"];
-            $ps=$_GET["pass"];
+            $un=$_GET["username"];
+            $ps=$_GET["password"];
             if(isset($_POST["username"]) && isset($_POST["password"])){
                 if($un==$_POST["username"] && $ps==$_POST["password"]){
-                    header("Location: http://localhost/Getspon/Home_page.php");
+                    header("Location: http://localhost/getspon/Home_page.php");
                 }else{
                    $nameErr="Invalid credentials";
                }
             }
         ?>
-<Text>Don't have an account? <a href="http://localhost/Getspon/Signup.php">Sign Up</a></Text><br><br>
+<Text>Don't have an account? <a href="http://localhost/getspon/Signup.php">Sign Up</a></Text><br><br>
 
 <Text>-------------------------OR-------------------------</Text><br><br>
 
 <button class="loginwithg-button">Login with Google</button>
 <button class="loginwithf-button">Login with Facebook</button>
     </div>
-
 </body>
 </html>
