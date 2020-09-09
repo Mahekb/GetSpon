ce13bbda994424aba89ba746d3dcd375c8b69482
Mahekb <51280331+Mahekb@users.noreply.github.com>
2020-09-09 16:25:52 +0530

    Update login.php

--- login.php
+++ login.php
@@ -22,10 +22,7 @@
         <li><a class="left" href="http://localhost/Getspon/Home_page.php">Home</a></li>
         <li><a class="left" href="#About">About</a></li>
         <li><a class="left" href="#Contact">Contact</a></li>
-        <li><a class="right" href="Log out">Log out</a></li>
         <li><a class="right" href="http://localhost/Getspon/Signup.php">Sign up</a></li>
-        <li><a class="right" href="http://localhost/Getspon/Login.php">Log in</a></li>
-
 </ul><br><br>
 
     <div align="center" id="log">
