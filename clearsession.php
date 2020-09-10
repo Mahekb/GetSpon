804b05902a39045f0b8ffc188c3d581e3a1203bd
Jainam321 <59683462+Jainam321@users.noreply.github.com>
2020-09-10 14:07:41 +0530

    Update clearsession.php

--- clearsession.php
+++ clearsession.php
@@ -1,1 +1,22 @@
+<?php 
+session_start();
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head></head>
+<body>
+
+<?php 
+
+session_unset();
+session_destroy();
+
+header("Location: http://localhost/Getspon/Signup.php");
+exit();
+
+
+?>
+    
+</body>
+</html>
