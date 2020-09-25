8ab5531dd28122c2ecead0242667072618201385
Mahekb <51280331+Mahekb@users.noreply.github.com>
2020-09-25 19:04:08 +0530

    Update Chat.js

--- Chat.js
+++ Chat.js
@@ -1,4 +1,10 @@
+document.getElementById("log1").style.display = "none";
 document.getElementsByClassName('tab')[0]
         .addEventListener('click', function (event) {
-            alert('Hey');
+            document.getElementById("log2").style.display = "none";
+            document.getElementById("log1").style.display = "block";
         });
+function back(){
+    document.getElementById("log1").style.display = "none";
+    document.getElementById("log2").style.display = "block";
+}
