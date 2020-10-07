07eedfb243e2168dd304cab42b29c074365db882
Mahekb <51280331+Mahekb@users.noreply.github.com>
2020-10-07 11:16:15 +0530

    Update login.php

--- login.php
+++ login.php
@@ -5,20 +5,20 @@ session_start();
 <!DOCTYPE html>
 
 <?php
-if(isset($_COOKIE['email'])) {
-    $email = $_COOKIE['email'];
+if(isset($_GET['username'])) {
+    $userna = $_GET['username'];
 }
 else {
-    $email = "";
+    $userna = "";
 }
 
 ?>
 
 <?php
-    if (isset($_POST['emailid']) && $_SERVER["REQUEST_METHOD"] == "POST") {
-    $emaill = $_POST["emailid"];
+    if (isset($_POST['uname']) && $_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["uname"];
     $_SESSION["login"] = "visible" ;
-    $_SESSION["email"] = $emaill ;
+    $_SESSION["username"] = $username ;
     }
 
 
@@ -37,7 +37,6 @@ else {
 <ul>
         <li><a class="left"><img src="Images/Mainlogo.jpg" width="100" </a></li>
         <li><a class="right" href="http://localhost/Getspon/Signup.php">Sign up</a></li>
-        <li><a class="right" href="http://localhost/Getspon/Login.php">Log in</a></li>
 
 </ul><br><br>
 
@@ -46,7 +45,7 @@ else {
         <img src="Images/login.jpg" height = "150" width="150">
     <form method="post" enctype="multipart/form-data" autocomplete="on" >
     <form method=post >
-        <input type = "text"  name = "emailid" placeholder="Enter email-id" class="input-box" required value= <?php echo $email?>><br><br>
+        <input type = "text"  name = "uname" placeholder="Enter Username" class="input-box" required value= <?php echo $userna?>><br><br>
         <input type="password" name="passwordid" placeholder="Enter password" class="input-box" required><br><br>
     <Text class="right"><a href="http://localhost/Getspon/Signup.php">Forgot Password?</a></Text><br><br>
        
@@ -59,10 +58,10 @@ else {
    
 <Text>Don't have an account? <a href="http://localhost/Getspon/Signup.php">Sign Up</a></Text><br><br>
 <?php
-            $un=$email;
+            $un=$userna;
             $ps=$_GET['password'];
-            if(isset($_POST["emailid"]) && isset($_POST["passwordid"])){
-                if($un==$_POST["emailid"] && $ps==$_POST["passwordid"]){
+            if(isset($_POST["uname"]) && isset($_POST["passwordid"])){
+                if($un==$_POST["uname"] && $ps==$_POST["passwordid"]){
                     header("Location: http://localhost/Getspon/Home_page.php");
                 }else{
                    $nameErr="Invalid credentials";
