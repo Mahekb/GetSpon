04a57f8e0d340706da9d4e080294f4271cb3c966
Jainam321 <59683462+Jainam321@users.noreply.github.com>
2020-12-03 17:22:39 +0530

    Update Events.php

--- Events.php
+++ Events.php
@@ -16,7 +16,7 @@ session_start();
 
 <?php 
 
-$enameErr = $cityErr = $stateErr = $addErr =  $phoneErr = $phone2Err = $emailErr =  "";
+$enameErr = $cityErr = $stateErr = $detErr = $amountErr = "";
 
 
 if (isset($_POST['eventname']) && $_SERVER["REQUEST_METHOD"] == "POST") {
@@ -26,25 +26,25 @@ if(isset($_POST['eventname'])) {
         if (empty($_POST["eventname"])) {
                 $enameErr = "Event Name is required";
         } 
-        else if (!preg_match("/^[a-zA-Z]*$/",$fname)) {
+        else if (!preg_match("/^[a-zA-Z]*$/",$_POST['eventname'])) {
                 $enameErr = "Only letters are allowed.";
-        } 
+        }  
         else {
                 $ename = $_POST["eventname"];
         }
 }   
      
 
-if(isset($_POST['address'])) {
+if(isset($_POST['details'])) {
 
-        if (empty($_POST["address"])) {
-                $addErr = "Address is required";
+        if (empty($_POST["details"])) {
+                $detErr = "Short detail is required";
         } 
-        else if (preg_match("/[^a-zA-Z0-9, .()-]/",$address)) {
-                $addErr = "Special Characters are not allowed.";
+        else if (preg_match("/[^a-zA-Z0-9, .]/",$_POST["details"])) {
+                $detErr = "Special Characters are not allowed.";
         }
         else {
-                $address = $_POST["address"];
+                $details = $_POST["details"];
         }
 }
 
@@ -68,67 +68,51 @@ if(isset($_POST['state'])) {
         }
 }
 
-if(isset($_POST['phoneno'])) {
+if(isset($_POST['amount'])) {
 
-        if (empty($_POST["phoneno"])) {
-                $phoneErr = "Phone number is required";
+        if (empty($_POST["amount"])) {
+                $amountErr = "Enter amount value";
         } 
-      else {
-                $phoneno = $_POST["phoneno"];
+        else if ((int)$_POST["amount"] < 10000 or (int)$_POST["amount"] > 200000) {
+                $amountErr = "Amount should be between 10,000 and 200,000.";
+        }
+        else {    
+                $amount =  (int)$_POST["amount"];
         }
 }
 
-if(isset($_POST['phoneno2'])) {
-
-    if (empty($_POST["phoneno2"])) {
-            $phone2Err = "Alternate Phone number is required";
-    } 
-  else {
-            $phoneno = $_POST["phoneno2"];
-    }
-}
-
-if(isset($_POST['email'])) {
-
-        if (empty($_POST["email"])) {
-                $emailErr = "Email is required";
-        } 
-      else {
-                $email = $_POST["email"];
-        }
-}
-
-      
-
-
-if($enameErr == "" && $cityErr == "" && $stateErr == "" && $addErr == "" && $ $phoneErr == "" && $phone2Err == "" && $emailErr == "") {
-
+if($enameErr == "" && $cityErr == "" && $stateErr == "" && $detErr == "") {
+        
         $conn = mysqli_connect("localhost","root","","Getspon");
         
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }
         $uname=$_SESSION['username'];
-        
+
+        $stmt = $conn->prepare("SELECT Phoneno, Email FROM user_details WHERE Username=?");
+        $stmt->bind_param('s', $uname);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $row = $result->fetch_assoc();
+
+        $pno = $row['Phoneno'];
+        $email = $row['Email'];
+        $stmt->close();
+
         $query = "INSERT INTO Events VALUES (?,?,?,?,?,?,?,?)";
-
         $pst = mysqli_prepare($conn,$query);
-
-        mysqli_stmt_bind_param($pst,"sssssiis",$uname,$ename,$address,$city,$state,$phoneno,$phoneno2,$email);
+        mysqli_stmt_bind_param($pst,"ssssssss",$uname,$ename,$details,$city,$state,$pno,$email,$amount);
 
         mysqli_stmt_execute($pst);	
-
         $getResult = mysqli_stmt_get_result($pst);	
-
         mysqli_stmt_close($pst);
-
         $conn->close();
 
-
         header("Location: http://localhost/Getspon/Home_page.php");
+
 exit();
 }
-
 }
 
 ?>
@@ -142,9 +126,9 @@ exit();
         <span class="error">* <?php echo $enameErr;?></span>
         <br><br>
         
-        Event Address:<br>
-        <textarea type = "textarea"  name = "address" class="input-box" rows="3" cols = "30" ></textarea>
-        <span class="error">* <?php echo $addErr;?></span>
+        Event short description:<br>
+        <textarea type = "textarea"  name = "details" class="input-box" rows="3" cols = "30" ></textarea>
+        <span class="error">* <?php echo $detErr;?></span>
         <br><br>
 City:
 <select name="city" class="input-box" size=1 >
@@ -157,7 +141,6 @@ City:
 <span class="error">* <?php echo $cityErr;?></span>
 <br><br>
 
-
 State:
 <select name="state" class="input-box" size=1>
 <option value="--select--">--select--</option>
@@ -169,21 +152,10 @@ State:
 <span class="error">* <?php echo $stateErr;?></span>
 <br><br>
 
-
-        Phone no:
-        <input type = "text"  name = "phoneno" class="input-box" >
-        <span class="error">* <?php echo $phoneErr;?></span>
-        <br><br>
-
-        Alternate Phone no:
-        <input type = "text"  name = "phoneno2" class="input-box" >
-        <span class="error">* <?php echo $phone2Err;?></span>
-        <br><br>
-Email:
-<input type="text" name="email" placeholder="username@gmail.com" class="input-box"> 
-<span class="error">* <?php echo $emailErr;?></span>
+Enter amount:
+<input type = "text"  name = "amount" class="input-box">
+<span class="error">* <?php echo $amountErr;?></span>
 <br><br>
-
        
 <input class="reset-button" type="reset" value="Reset">
 <input class="submit-button" type="submit" value="Register">
@@ -195,5 +167,4 @@ Email:
 
 </body>
 </html>
-
 
