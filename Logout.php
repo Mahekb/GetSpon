1a43cb7dcb11ed2aed21ce98bfa44f874e1623a9
Jainam321 <59683462+Jainam321@users.noreply.github.com>
2020-12-15 23:08:15 +0530

    Update Logout.php

--- Logout.php
+++ Logout.php
@@ -6,29 +6,35 @@ session_start();
   $islogin="hidden";
   $islogout="visible";
 
-  if(isset($_SESSION['email']) && isset($_SESSION['login'])){
+  if(isset($_SESSION['username']) && isset($_SESSION['login'])){
     $islogin=$_SESSION['login'];
     $islogout="hidden";
   }
 ?>
 
+<?php 
+
+$conn = mysqli_connect("localhost","root","","Getspon");
+        
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        $uname=$_SESSION['username'];
+
+        $stmt2 = $conn->prepare("SELECT Firstname,Lastname FROM user_details WHERE Username=?");
+        $stmt2->bind_param('s', $uname);
+        $stmt2->execute();
+        $result2 = $stmt2->get_result();
+        while ($row = $result2->fetch_assoc()) { 
+            $fn = $row['Firstname'];
+            $ln = $row['Lastname'];
+        }
+        $stmt2->close();
+        $conn->close();
+?>
+
 <!DOCTYPE html>
 
-<?php
-if(isset($_COOKIE['firstname'])) {
-    $fname = $_COOKIE['firstname'];
-}
-else {
-    $fname = "";
-}
-
-if(isset($_COOKIE['lastname'])) {
-    $lname = $_COOKIE['lastname'];
-}
-else {
-    $lname = "";
-}
-?>
 
 <html lang="en">
 <head>
@@ -47,17 +53,21 @@ else {
         <li><a class="left" href="http://localhost/Getspon/Home_page.php">Home</a></li>
         <li><a class="left" href="#About">About</a></li>
         <li><a class="left" href="#Contact">Contact</a></li>
-        <li style="visibility:<?php echo "$islogin"?>"><a class="right" href="http://localhost/Getspon/Chat.php">Chat</a></li>
+        
+        <li style="visibility:<?php echo "$islogin"?>"><a class="right" href="http://localhost/Getspon/profilepage.php">Profile</a></li>
         <li style="visibility:<?php echo "$islogin"?>"><a class="right" href="http://localhost/Getspon/Logout.php">Log out</a></li>
+        <li style="visibility:<?php echo "$islogin"?>"><a class="right" href="http://localhost/Getspon/Chat.php">Chat</a></li>
+        <li style="visibility:<?php echo "$islogin"?>"><a class="right" href="http://localhost/Getspon/Startup.php">Add your Startup</a></li>
+        <li style="visibility:<?php echo "$islogin"?>"><a class="right" href="http://localhost/Getspon/Events.php">Add new Event</a></li>
         <li style="visibility:<?php echo "$islogout"?>"><a class="right" href="http://localhost/Getspon/Signup.php">Sign up</a></li>
         <li style="visibility:<?php echo "$islogout"?>"><a class="right" href="http://localhost/Getspon/Login.php">Log in</a></li>
 
-</ul><br><br>
+</ul><br/>
 
     <div align="center" id="log">
         <h1>Logout</h1>
         <img src="Images/login.jpg" height = "150" width="150"></br>
-        <h1 class="uppercase"><?php echo $fname . " " . $lname?></h1></br></br>
+        <h1 class="uppercase"><?php echo $fn . " " . $ln?></h1></br></br>
         <form method="post" action="clearsession.php" >
     <button class="button" type="submit" name="logout">Logout</button><br><br>
 </form>
