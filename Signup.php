78c5d24317a160cc9836a71af2c6c55983d29001
Jainam <jainam.tm@somaiya.edu>
2021-09-17 01:43:09 +0530

    signup page bugs fixed

--- Signup.php
+++ Signup.php
@@ -15,6 +15,10 @@
 
         <?php
 
+        $uname = $dob = $gender = $username = $phoneno = $email = $password = $cpassword = "";
+        $checkbox = "";
+        $city = $state = '--select--';
+
         $unameErr = $genderErr = $cityErr = $stateErr = $dobErr = "";
         $userErr = $phoneErr = $emailErr = $passErr = $conpassErr = $cpassErr = $checkboxErr = "";
 
@@ -22,18 +26,16 @@
         if (isset($_POST['uname']) && $_SERVER["REQUEST_METHOD"] == "POST") {
 
                 if (isset($_POST['uname'])) {
-
+                        $uname = $_POST["uname"];
                         if (empty($_POST["uname"])) {
                                 $unameErr = "Name is required";
                         } else if (!preg_match("/^[a-zA-Z ]*$/", $_POST["uname"])) {
                                 $unameErr = "Only letters are allowed.";
-                        } else {
-                                $uname = $_POST["uname"];
                         }
                 }
 
                 if (isset($_POST['dateofbirth'])) {
-
+                        $dob = $_POST["dateofbirth"];
                         if (empty($_POST["dateofbirth"])) {
                                 $dobErr = "DOB is required";
                         } else {
@@ -43,30 +45,21 @@
                                 $diff = $today->diff($dateob);
                                 if ($diff->y < 18) {
                                         $dobErr = "You are not eligible.";
-                                } else {
-                                        $dob = $_POST['dateofbirth'];
                                 }
-
-
-                                $dob = $_POST["dateofbirth"];
                         }
                 }
 
                 if (isset($_POST['city'])) {
-
+                        $city = $_POST["city"];
                         if ($_POST['city'] == "--select--") {
                                 $cityErr = "City is required";
-                        } else {
-                                $city = $_POST["city"];
                         }
                 }
 
                 if (isset($_POST['state'])) {
-
+                        $state = $_POST["state"];
                         if ($_POST['state'] == "--select--") {
                                 $stateErr = "State is required";
-                        } else {
-                                $state = $_POST["state"];
                         }
                 }
 
@@ -78,49 +71,38 @@
 
 
                 if (isset($_POST['username'])) {
+                        $username = $_POST["username"];
 
                         if (empty($_POST["username"])) {
                                 $userErr = "Username is required";
                         } else if (!preg_match("/^[a-zA-Z0-9]*$/", $_POST["username"])) {
                                 $userErr = "Only letters and numbers are allowed.";
-                        } else if (strlen($_POST["username"]) <= 6) {
-                                $userErr = "Username must be longer than 6 characters";
-                        } else if (strlen($_POST["username"]) > 20) {
-                                $userErr = "Username must be not longer than 20 characters";
-                        } else {
-                                $username = $_POST["username"];
                         }
                 }
 
                 if (isset($_POST['phoneno'])) {
-
+                        $phoneno = $_POST["phoneno"];
                         if (empty($_POST["phoneno"])) {
                                 $phoneErr = "Phone number is required";
                         } else if (!preg_match("/\d{10}/", $_POST["phoneno"])) {
                                 $phoneErr = "Invalid Phone number.";
-                        } else {
-                                $phoneno = $_POST["phoneno"];
                         }
                 }
 
 
                 if (isset($_POST['email'])) {
-
+                        $email = $_POST["email"];
                         if (empty($_POST["email"])) {
                                 $emailErr = "Email is required";
                         } else if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
                                 $emailErr = "Invalid email format";
-                        } else {
-                                $email = $_POST["email"];
                         }
                 }
 
                 if (isset($_POST['password'])) {
-
+                        $password = $_POST["password"];
                         if (empty($_POST["password"])) {
                                 $passErr = "Password is required";
-                        } else if (strlen($_POST["password"]) <= 6) {
-                                $passErr = "Password must be atleast of 7 characters";
                         } else if (!preg_match("/[A-Z]{1,}/", $_POST["password"])) {
                                 $passErr = "Atleast one uppercase letter is required in password.";
                         } else if (!preg_match("/[a-z]{1,}/", $_POST["password"])) {
@@ -132,28 +114,27 @@
                         } else if (preg_match("/[ ]{1,}/", $_POST["password"])) {
                                 $passErr = "Spaces are not allowed in password.";
                         } else {
-                                $password = $_POST["password"];
                         }
                 }
 
                 if (isset($_POST['cpassword'])) {
 
+                        $cpassword = $_POST["cpassword"];
                         if (empty($_POST["cpassword"])) {
                                 $conpassErr = "Password needs to be entered again.";
-                        } else {
-                                $cpassword = $_POST["cpassword"];
+                        } else if ($_POST['password'] != $_POST['cpassword']) {
+                                $conpassErr = "Passwords should be same.";
                         }
                 }
 
-                if ($_POST['password'] != $_POST['cpassword']) {
-                        $conpassErr = "Passwords should be same.";
-                }
 
+                if (!empty($_POST["checkbox"])) {
+                        $checkbox = $_POST['checkbox'];
+                }
 
                 if (empty($_POST["checkbox"])) {
                         $checkboxErr = "Terms and conditions needs to be agreed.";
                 }
-
 
                 if ($unameErr == "" && $genderErr == "" && $cityErr == "" && $stateErr == "" && $dobErr == "" && $userErr == "" && $phoneErr == "" && $emailErr == "" && $passErr == "" && $conpassErr == "" && $checkboxErr == "" && $cpassErr == "") {
 
@@ -201,73 +182,119 @@
 
                 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                         Name:
-                        <input type="text" name="uname" class="input-box" placeholder="First_Name Last_Name">
-                        <span class="error">* <?php echo $unameErr; ?></span>
+                        <input type="text" name="uname" class="input-box" value="<?php echo $uname ?>" placeholder="First_Name Last_Name" maxlength="40">
+                        <?php
+                        if ($unameErr != "") {
+                                echo '<br/> <span class="error">*' . $unameErr . '</span>';
+                        }
+                        ?>
+
                         <br><br>
 
                         Date of Birth:
-                        <input type="date" name="dateofbirth" class="input-box">
-                        <span class="error">* <?php echo $dobErr; ?></span>
+                        <input type="date" name="dateofbirth" class="input-box" value="<?php echo $dob ?>">
+                        <?php
+                        if ($dobErr != "") {
+                                echo '<br/> <span class="error">*' . $dobErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         City:
-                        <select name="city" class="input-box" size=1>
-                                <option value="--select--">--select--</option>
+                        <select name="city" class="input-box" size=1 value="<?php echo $city ?>">
+                                <option value="<?php echo $city ?>"><?php echo $city ?></option>
                                 <option value="Mumbai">Mumbai</option>
                                 <option value="Pune">Pune</option>
                                 <option value="Bangalore">Bangalore</option>
                                 <option value="Delhi">Delhi</option>
                         </select>
-                        <span class="error">* <?php echo $cityErr; ?></span>
+                        <?php
+                        if ($cityErr != "") {
+                                echo '<br/> <span class="error">*' . $cityErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         State:
-                        <select name="state" class="input-box" size=1>
-                                <option value="--select--">--select--</option>
+                        <select name="state" class="input-box" size=1 value="<?php echo $state ?>">
+                                <option value="<?php echo $state ?>"><?php echo $state ?></option>
                                 <option value="Gujarat">Gujarat</option>
                                 <option value="Maharashtra">Maharashtra</option>
                                 <option value="Punjab">Punjab</option>
                                 <option value="Rajasthan">Rajasthan</option>
                                 <option value="Tamil Nadu">Tamil Nadu</option>
                         </select>
-                        <span class="error">* <?php echo $stateErr; ?></span>
+                        <?php
+                        if ($stateErr != "") {
+                                echo '<br/> <span class="error">*' . $stateErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         Gender:
-                        <input type="radio" name="gender" value="male"> Male
-                        <input type="radio" name="gender" value="female"> Female
-                        <input type="radio" name="gender" value="other"> Other
-                        <span class="error">* <?php echo $genderErr; ?></span>
+                        <input type="radio" name="gender" value="male" <?php if ($gender == 'male') echo 'checked' ?>> Male
+                        <input type="radio" name="gender" value="female" <?php if ($gender == 'female') echo 'checked' ?>> Female
+                        <input type="radio" name="gender" value="other" <?php if ($gender == 'other') echo 'checked' ?>> Other
+                        <?php
+                        if ($genderErr != "") {
+                                echo '<br/> <span class="error">*' . $genderErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         Username:
-                        <input type="text" name="username" class="input-box">
-                        <span class="error">* <?php echo $userErr; ?></span>
+                        <input type="text" name="username" class="input-box" value="<?php echo $username ?>" minlength="6" maxlength="20">
+                        <?php
+                        if ($userErr != "") {
+                                echo '<br/> <span class="error">*' . $userErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         Phone no:
-                        <input type="text" name="phoneno" class="input-box">
-                        <span class="error">* <?php echo $phoneErr; ?></span>
+                        <input type="text" name="phoneno" class="input-box" value="<?php echo $phoneno ?>">
+                        <?php
+                        if ($phoneErr != "") {
+                                echo '<br/> <span class="error">*' . $phoneErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         Email:
-                        <input type="text" name="email" placeholder="username@gmail.com" class="input-box">
-                        <span class="error">* <?php echo $emailErr; ?></span>
+                        <input type="text" name="email" placeholder="username@gmail.com" class="input-box" value="<?php echo $email ?>" maxlength="30">
+                        <?php
+                        if ($emailErr != "") {
+                                echo '<br/> <span class="error">*' . $emailErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         Password:
-                        <input type="password" name="password" class="input-box" placeholder="Create a strong password">
-                        <span class="error">* <?php echo $passErr; ?></span>
+                        <input type="password" name="password" class="input-box" placeholder="Create a strong password" value="<?php echo $password ?>" minlength="7" maxlength="30">
+                        <?php
+                        if ($passErr != "") {
+                                echo '<br/> <span class="error">*' . $passErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         Confirm password:
-                        <input type="password" name="cpassword" class="input-box" placeholder="Make sure to enter the same password">
-                        <span class="error">* <?php echo $conpassErr; ?></span>
+                        <input type="password" name="cpassword" class="input-box" placeholder="Make sure to enter the same password" value="<?php echo $cpassword ?>">
+                        <?php
+                        if ($conpassErr != "") {
+                                echo '<br/> <span class="error">*' . $conpassErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         <Text>By creating an account you agree to our <sa href="#">Terms & Privacy</sa>.</Text><br><br>
-                        <input type="checkbox" name="checkbox" value="checkbox">I agree to the terms and conditions.
-                        <span class="error">* <?php echo $checkboxErr; ?></span>
+                        <input type="checkbox" name="checkbox" value="checkbox" <?php if ($checkbox == 'checkbox') echo 'checked' ?>>
+                        I agree to the terms and conditions.
+                        <?php
+                        if ($checkboxErr != "") {
+                                echo '<br/> <span class="error">*' . $checkboxErr . '</span>';
+                        }
+                        ?>
                         <br><br>
 
                         <input class="reset-button" type="reset" value="Reset">
